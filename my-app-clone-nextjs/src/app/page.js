640c9d3ba81963031edead1e2b6fc09427a30860
thomasMinh1995 <thomasMinh1995@gmail.com>
2024-12-17 17:08:46 +0700

    Dec-17: add json-server and fist fetch data header

--- my-app-clone-nextjs/src/app/page.js
+++ my-app-clone-nextjs/src/app/page.js
@@ -1,8 +1,19 @@
 
-export default function Home() {
+export default async function Home() {
+
+  const res = await fetch('http://localhost:3004/header')
+  const header = await res.json()
+
+  const items = header.items;
+  
+  console.log('Fetched items:', header);
   return (
     <div>
-      <h1>Hello world</h1>
+      {
+        items.map((item) => (
+          <li>{item.title}</li>
+        ))
+      }
     </div>
   );
 }
